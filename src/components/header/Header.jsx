3afea35a9cd5369b9806c297e8e2bbafdedc5dc2
Danil <longstmia@yandex.ru>
2024-10-22 23:35:45 +0500

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.scss'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <p>Курсы</p>
+        <p>Курсы</p>
+      </nav>
+    </header>
+  )
+}
