d18078004aaaca3f34adf6413d162ad2c0877458
Danil <longstmia@yandex.ru>
2024-10-23 22:49:35 +0500

    new-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import React from 'react'
-import Header from './components/header/Header'
 import 'normalize.css'
 import './styles/index.scss'
 
 function App() {
-  return (
-    <>
-      <Header />
-    </>
-  )
+  return <></>
 }
 
 export default App
